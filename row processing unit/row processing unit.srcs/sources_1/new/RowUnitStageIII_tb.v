f16ae6b3c3a9fa3f0ed7010d9e60006e80e4c255
Sayed Muhin <sdmuhsin@gmail.com>
2020-10-04 11:56:04 +0530

    Row unit test bench

--- row processing unit/row processing unit.srcs/sources_1/new/RowUnitStageIII_tb.v
+++ row processing unit/row processing unit.srcs/sources_1/new/RowUnitStageIII_tb.v
@@ -32,37 +32,46 @@ module RowUnitStageIII_tb(
     //output [Wc*W-1:0]LLR_OUT;
     reg [Wc*W-1:0]LLR_IN;
     reg [ECOMPSIZE-1:0]E_MEM_IN;
+    reg clk,rst;
     
-    wire [Wc-1:0]SIGN;
-    wire [Wc*(W-1)-1:0]ABS;
+    wire [Wc*W-1:0]LLR_OUT;
+    wire [ECOMPSIZE-1:0]E_MEM_OUT;
     
-    wire [W-2:0]ABS_DISPLAY[Wc-1:0];
+    //wire [Wc-1:0]SIGN;
+    //wire [Wc*(W-1)-1:0]ABS;
+    
+    //wire [W-2:0]ABS_DISPLAY[Wc-1:0];
     wire [W-1:0]LLR_IN_DISPLAY[Wc-1:0];
+    wire [W-1:0]LLR_OUT_DISPLAY[Wc-1:0];
     
-    wire [W-2:0]MIN1_DISPLAY;
-    wire [W-2:0]MIN2_DISPLAY;
-    wire [4:0]INDEX_DISPLAY; // 5 bit
-    wire [Wc-1:0]SIGNS_IN_DISPLAY;
+    wire [W-2:0]ECOMP_IN_MIN1_DISPLAY;
+    wire [W-2:0]ECOMP_IN_MIN2_DISPLAY;
+    wire [4:0]ECOMP_IN_INDEX_DISPLAY; // 5 bit
+    wire [Wc-1:0]ECOMP_IN_SIGNS_IN_DISPLAY;
+        
+    wire [W-2:0]ECOMP_OUT_MIN1_DISPLAY;
+    wire [W-2:0]ECOMP_OUT_MIN2_DISPLAY;
+    wire [4:0]ECOMP_OUT_INDEX_DISPLAY; // 5 bit
+    wire [Wc-1:0]ECOMP_OUT_SIGNS_IN_DISPLAY;
     
-    assign {MIN1_DISPLAY,MIN2_DISPLAY,INDEX_DISPLAY,SIGNS_IN_DISPLAY} = E_MEM_IN;
+    assign {ECOMP_IN_MIN1_DISPLAY,ECOMP_IN_MIN2_DISPLAY,ECOMP_IN_INDEX_DISPLAY,ECOMP_IN_SIGNS_IN_DISPLAY} = E_MEM_IN;
+    assign {ECOMP_OUT_MIN1_DISPLAY,ECOMP_OUT_MIN2_DISPLAY,ECOMP_OUT_INDEX_DISPLAY,ECOMP_OUT_SIGNS_IN_DISPLAY} = E_MEM_IN;
     
-   genvar i;
-   generate for(i=0;i<Wc;i=i+1)begin:l1
+    genvar i;
+    generate for(i=0;i<Wc;i=i+1)begin:l1
         assign LLR_IN_DISPLAY[i][W-1:0] = LLR_IN[(i+1)*W-1:i*W];
-        assign ABS_DISPLAY[i][W-2:0] = ABS[(i+1)*(W-1)-1:i*(W-1)];
-   end
-   endgenerate
-    
-    //Internal signals
-    wire [Wc*W-1:0]SUB_OUT;
-    wire [Wc*W-1:0]REC_1_OUT;
-    
-    RowUnit r(SIGN,ABS,E_MEM_IN,LLR_IN);
-    
-    assign SUB_OUT = r.SUB_OUT2;
-    assign REC_1_OUT = r.REC_1_OUT;
+        assign LLR_OUT_DISPLAY[i][W-1:0] = LLR_OUT[(i+1)*W-1:i*W];
+        //assign ABS_DISPLAY[i][W-2:0] = ABS[(i+1)*(W-1)-1:i*(W-1)];
+    end
+    endgenerate
+
+    RowUnit ru(LLR_OUT,E_MEM_OUT,E_MEM_IN,LLR_IN,clk,rst);
     initial begin
         LLR_IN = 0;
         E_MEM_IN = 0;
+        
+        clk = 0;
+        rst = 0;
     end
+    always #5 clk = ~clk;
 endmodule
