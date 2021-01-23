dcab8eb6c64cdb25d624af1f001e261de3cac94e
Sayed Muhin <sdmuhsin@gmail.com>
2021-01-23 11:20:54 +0530

    Now it works for trivial input

--- verification/ne_decoder_verif/func_full_run.m
+++ verification/ne_decoder_verif/func_full_run.m
@@ -38,7 +38,10 @@ function [hlut,E,L,D] = func_full_run ( cdwrd, max_iterns)
      # E : 1022 x 32
      # D : 1x8176
      L(1:10)
+     cnt = 0;
      for ly = 1:1:lys
+      
+      fprintf("start of layer = %d, min(L) = %d, max(L) = %d max of D = %d\n", ly,min(L),max(L),max(max(D))); 
       
       for slice = 1:1:max_slices
         #Gather all rows involved in this slice
@@ -51,9 +54,9 @@ function [hlut,E,L,D] = func_full_run ( cdwrd, max_iterns)
         rows_global = (ly-1)*z .+ rows;
         
         # Outputs of p RCUs
-        # L_out 26 x 32 
+        # Update a certain ammount of symbols of L, max 26 x 32 symbols
         # E -> update a 26 x 32 chunk
-               
+        
         for row = rows_global
           
           symbol_indices = hlut(row,:);
@@ -77,7 +80,7 @@ function [hlut,E,L,D] = func_full_run ( cdwrd, max_iterns)
           E_in = E(row,:); # 1 x (1 + 1 + 1 + 32)
           L_in = symbols;
           D_in = D(symbol_indices);
-          
+
           rec_1_out = func_module_recover(E_in); # 1 x 32
           
           Q = func_saturate(L_in - rec_1_out);
@@ -87,14 +90,30 @@ function [hlut,E,L,D] = func_full_run ( cdwrd, max_iterns)
           [m1,m2,m1_psn] = func_module_minfinder(Q_abs);
           
           E_out = [m1,m2,m1_psn,Q_sign];
+          
+          #fprintf(" --Q = %d, min(Q) = %d, m1 = %d, E_out = %d \n",Q_abs(1),min(Q_abs),m1,E_out(1));
           rec_2_out = func_module_recover(E_out);
           L_out = func_saturate( Q_abs + rec_2_out + D_in );
           D_out = func_saturate( rec_2_out - rec_1_out);
           
+          if(iteration == 1 &&  sum(L_in) ~= length(rec_2_out) * rec_2_out(1) &&row < 513 )
+            fprintf("iteration = %d, row = %d \n",iteration,row);
+            symbol_indices
+            L_in
+            D_in
+            rec_1_out
+            Q_abs
+            Q_sign
+            rec_2_out
+            L_out
+            D_out
+          endif
+        
           # Update to global L 1x8176, global E 1022x32, global D 1x8176
           # Iterate accross all 32 symbols
-          for i = 1:1:size(symbol_indices)(1)
-            #Check if reacess
+          for i = 1:1:size(symbol_indices)(2)
+            #fprintf("i=%d,",i);
+            #Check if symbol is first access or reacess
             if(reaccess_mask(row,i) == 1) #First access
                #Update D
                D(symbol_indices(i)) = D_out(i);
@@ -104,16 +123,20 @@ function [hlut,E,L,D] = func_full_run ( cdwrd, max_iterns)
                D(symbol_indices(i)) = 0;
                #Update L
                L(symbol_indices(i)) = L_out(i);
+               cnt += 1;
             else
               fprintf("This shouldn't happen, reaccess_mask");
             endif
             
-          endfor      
+          endfor   
           #Update E
           E(row,:) = E_out;
-        endfor
-      
-      endfor
-     endfor
-  endfor
+        endfor # row end
+        if(iteration == 100)
+        fprintf(" iter = %d, L_in = %d, L_out = %d, rec_2_out - rec_1_out = %d - %d \n",iteration, L_in(1),L_out(1),rec_2_out(1),rec_1_out(1));      
+        endif
+      endfor #slice end
+     endfor#layer end
+  cnt
+  endfor#iteration end
 endfunction
